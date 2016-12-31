3497a80ca37e2da7c60a8d7b5e3adb1d9df32d4c
Tim Rijckaert <tim.rijckaert@vrt.be>
2016-12-31 16:23:42 +0100

    Configured remote debugging with PyCharm IDEA pydevd a python file which allows you to  start tracing when in debug mode

--- addon.py
+++ addon.py
@@ -1,9 +1,14 @@
 import xbmcaddon, xbmcgui
+from resources.lib.utils.debugger import startdebugger
+
+debug = True
+if debug:
+    startdebugger()
 
 addon = xbmcaddon.Addon()
 addonname = addon.getAddonInfo('name')
 
-line1 = "Hello World!"
+line1 = "Hello World Tim!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
 
