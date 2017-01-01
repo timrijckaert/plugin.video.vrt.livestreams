729f853f4a8b7fb495188e70991afb04419b2a64
Tim Rijckaert <tim.rijckaert@vrt.be>
2017-01-01 16:05:52 +0100

    Added debug log for testing on Raspberry Pi

--- addon.py
+++ addon.py
@@ -1,3 +1,5 @@
+import sys
+
 import xbmc
 import xbmcaddon
 import xbmcgui
@@ -6,6 +8,8 @@ from resources.lib.utils.debugger import start_debugger
 start_debugger()
 
 if __name__ == '__main__':
+    message = "sys.argv 0: %s 1: %s 2: %s" % (sys.argv[0], sys.argv[1], sys.argv[2])
+    xbmcgui.Dialog().notification(heading="debug", message=message)
     addon = xbmcaddon.Addon('plugin.video.kodiplayground')
     icon = addon.getAddonInfo('icon')
     link = "http://live.stream.vrt.be/vrt_stubru_live/stream1296/playlist.m3u8"
@@ -13,6 +17,3 @@ if __name__ == '__main__':
     list_item = xbmcgui.ListItem(label=title, iconImage=icon, thumbnailImage=icon, path=link)
     list_item.setInfo(type='Video', infoLabels={"Title": title})
     list_item.setProperty("isPlayable", 'true')
-
-    player = xbmc.Player()
-    player.play(item=link, listitem=list_item)
